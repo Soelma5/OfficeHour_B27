00e70efb716a38fd9e9ab0799113c732b6766245
Soelma5 <sanzhieva2020@gmail.com>
2022-08-25 23:56:15 -0400

    Added one more solving task2

---
+++ src/day08_08_24_2022/Task2.java
@@ -0,0 +1,25 @@
+package day08_08_24_2022;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        ArrayList<String> passwords = new ArrayList<>(Arrays.asList("one", "hi", "hold"));
+        ArrayList<String> hiddenPasswords = new ArrayList<>();
+
+        for(String password : passwords){
+            String hiddenPassword = hidePassword(password, '*');
+            hiddenPasswords.add(hiddenPassword);
+        }
+        System.out.println("Hidden password: " + hiddenPasswords);
+    }
+
+    private static String hidePassword(String s, char c){
+        String hiddenPassword = "";
+        for(int i = 0; i < s.length(); i++){
+           hiddenPassword += c;
+        }
+        return hiddenPassword;
+    }
+}
